cf36b84b52136cbc36d2da144c7a046f686a4512
Bestian Tang <bestian@gmail.com>
2020-10-14 15:20:04 +0800

    deploy

---
+++ precache-manifest.ced621efce38add0cc73a04b34899214.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f97d52f92a30dee70f8",
+    "url": "/basketball/css/app.1f548088.css"
+  },
+  {
+    "revision": "67e58f7c452a5ba9edba784f1e65e3aa",
+    "url": "/basketball/happy.mp3"
+  },
+  {
+    "revision": "afa8be4aa12c6d6408cbb6a2400a2188",
+    "url": "/basketball/img/ball.afa8be4a.png"
+  },
+  {
+    "revision": "afa80630c5cd02e1223b6689e5beeb1b",
+    "url": "/basketball/img/ball.png"
+  },
+  {
+    "revision": "087434a4050e2897d8ce72bf37fde0ed",
+    "url": "/basketball/img/basket.087434a4.jpg"
+  },
+  {
+    "revision": "5b36670bd50005242c0f99646b158297",
+    "url": "/basketball/img/櫻木.5b36670b.jpeg"
+  },
+  {
+    "revision": "e19a44e3cfe4609662b98153630f49f8",
+    "url": "/basketball/img/櫻木灌籃.e19a44e3.jpeg"
+  },
+  {
+    "revision": "fe5064419875dd750024c15c106050c9",
+    "url": "/basketball/img/流川.fe506441.jpeg"
+  },
+  {
+    "revision": "4a56dbe042a19dc952b6e84ff1ee34bb",
+    "url": "/basketball/img/流川灌籃.4a56dbe0.jpeg"
+  },
+  {
+    "revision": "1a44b68123efe398171b9e67d1aab96c",
+    "url": "/basketball/index.html"
+  },
+  {
+    "revision": "bb010f3bf334674acd4e",
+    "url": "/basketball/js/about.5c54de32.js"
+  },
+  {
+    "revision": "5f97d52f92a30dee70f8",
+    "url": "/basketball/js/app.7b1856ad.js"
+  },
+  {
+    "revision": "921c35a26e81fe52d76a",
+    "url": "/basketball/js/chunk-vendors.8ff22080.js"
+  },
+  {
+    "revision": "31bca525f9370bfbbc4ef9d9ab83f7b5",
+    "url": "/basketball/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/basketball/robots.txt"
+  }
+]);
